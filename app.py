43c8a77ee6d5c15cea5e8f016a3f1a9a80cfb766
Jagath Kiran <jagathkiran14569@gmail.com>
2025-06-13 23:10:19 +0530

    Added port

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-with open('house_price_prediction.pkl','rb') as f:
-    model=pickle.load(f);
-    
+with open('house_price_prediction.pkl', 'rb') as f:
+    model = pickle.load(f)
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -32,10 +33,11 @@ def predict():
     features_array = np.array([features])
     
     prediction = model.predict(features_array)
-    output = round(prediction[0],2)
+    output = round(prediction[0], 2)
     
     return render_template('index.html', prediction_text=f"Predicted Price: {output}")
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = 'PORT' not in os.environ
+    app.run(host='0.0.0.0', port=port, debug=debug)
